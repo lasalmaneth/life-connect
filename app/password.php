2d9d562180eec2ba287e4d8b4f0a85d796fe6443
sew0627 <sewminichanchala0627@gmail.com>
2026-04-10 22:38:55 +0530

    remove testing files.

--- app/password.php
+++
@@ -1,8 +0,0 @@
-<?php
-$rawPassword = "Admin@123";
-$hashedPassword = password_hash($rawPassword, PASSWORD_DEFAULT);
-echo $hashedPassword;
-?>
-
-<!-- all admins - Admin@123 
-other users- User@123 -->
--- database/update_custodians_schema.sql
+++
@@ -1,6 +0,0 @@
--- migration to refactor custodians table
-ALTER TABLE custodians DROP COLUMN custodian_number;
-ALTER TABLE custodians ADD COLUMN status VARCHAR(20) DEFAULT 'PENDING';
-
--- existing custodians are active
-UPDATE custodians SET status = 'ACTIVE' WHERE status IS NULL;
