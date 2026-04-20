015f2af4db79ae4a4c1a12f546e24476728ff44c
s-samarawickrama <sahasnasss@gmail.com>
2026-04-20 19:13:23 +0530

    alignment fix

--- app/views/custodian/dashboard.view.php
+++ app/views/custodian/dashboard.view.php
@@ -212,6 +212,7 @@ ob_start();
             <div class="cp-dashboard-grid mb-4">
 
                 <!-- [SECTION 2] DONATION OUTCOME & GUIDANCE CARD -->
+                <?php if ($mode === 'KIDNEY_ONLY' || ($hasKidney && ($hasCornea || $hasOther)) || $mode === 'BODY_PLUS_CORNEA'): ?>
                 <div class="col-span-1 md:col-span-2">
                     
                     <!-- SCENARIO 1/2: KIDNEY ONLY -->
@@ -390,6 +391,7 @@ ob_start();
 
                     <!-- [SECTION 3 & 4] ACTIONS & NOTICES RESTRUCTURING NEXT -->
                 </div>
+                <?php endif; ?>
 
                 <!-- [SECTION 3] OPERATIONAL ACTION AREA -->
                 
@@ -596,9 +598,12 @@ ob_start();
                 <?php endif; ?>
 
                 <!-- 3. SUPPORT SECTIONS -->
+                <?php if (!empty($custodianApprovals)): ?>
                 <div class="col-span-1 md:col-span-2">
                     <?php require __DIR__ . '/partials/co-custodian-approvals.php'; ?>
                 </div>
+                <?php endif; ?>
+                
                 <div class="col-span-1 md:col-span-2">
                     <?php require __DIR__ . '/partials/activity-timeline.php'; ?>
                 </div>
